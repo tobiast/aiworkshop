36fe1a3bbba3ae558ceae78db720c5f0d5025445
Tobias K Torrissen <tobiast@gmail.com>
2024-09-26 18:03:36 +0200

    feat: Add GET endpoint for retrieving motorcycle details

--- src/main/java/no/torrissen/motorcycles/MotorcyclesController.java
+++ src/main/java/no/torrissen/motorcycles/MotorcyclesController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MotorcyclesController {
 
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return "Hello, World!";
-    }
-
     @GetMapping("/motorcycle")
     public Motorcycle getMotorcycle() {
         return new Motorcycle("Street 750", "Harley-Davidson", 2023);
